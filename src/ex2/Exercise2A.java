c1d64e431a8dd7eee21929a7a52a27d1fa45cf10
Decane <nightcyberhunter@gmail.com>
2024-12-03 20:43:46 +0100

    [AOC 2A]

---
+++ src/ex2/Exercise2A.java
@@ -0,0 +1,67 @@
+package ex2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SuppressWarnings("java:S106")
+public class Exercise2A {
+
+    private static final List<SafetyReport> safetyList = new ArrayList<>();
+
+    private static boolean isAscending(List<Integer> nums) {
+
+        return IntStream.range(0, nums.size() - 1).noneMatch(i -> nums.get(i) < nums.get(i + 1));
+    }
+
+    private static boolean isDescending(List<Integer> nums) {
+        return IntStream.range(0, nums.size() - 1).noneMatch(i -> nums.get(i) > nums.get(i + 1));
+    }
+
+    public static void main(String[] args) {
+
+        try (var reader = new BufferedReader(new FileReader("src/ex2/input.txt"))) {
+
+            var line = reader.readLine();
+
+            while (null != line) {
+                var tokens = Arrays.stream(line.trim().split(" ")).map(Integer::parseInt).toList();
+                if (!hasWrongOrder(tokens) && !isDifferenceTooHigh(tokens)) {
+                    safetyList.add(SafetyReport.SAFE);
+                } else {
+                    safetyList.add(SafetyReport.UNSAFE);
+                }
+
+
+                line = reader.readLine();
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Reports: " + safetyList.stream().filter(SafetyReport.SAFE::equals).count());
+
+    }
+
+    private static boolean hasWrongOrder(List<Integer> tokens) {
+        return !isDescending(tokens) && !isAscending(tokens);
+    }
+
+    private static boolean isDifferenceTooHigh(List<Integer> tokens) {
+        for (int i = 0; i < tokens.size() - 1; i++) {
+            var difference = Math.abs(tokens.get(i) - tokens.get(i+1));
+
+            if (difference <= 0 || difference > 3) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
